a6b5a2ae6a41ce68522b38d8736a8fb3607663c4
Hilson <82933261+Hilsonxhero@users.noreply.github.com>
2022-08-14 10:54:47 +0430

    init user  and role permissions module

---
+++ Modules/RolePermissions/Database/Seeders/RolePermissionsDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\RolePermissions\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class RolePermissionsDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
