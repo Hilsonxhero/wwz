c085fcdffbadd75b85e31261683ac6a400b9b2e5
Hilson <hilsonxhero.me@gmail.com>
2023-05-21 00:21:33 +0330

    chore : dev

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('foo', function () {
+    echo "foo";
+    sleep(20);
+});
+
+Route::get('ping', function () {
+    echo "ping";
 });
